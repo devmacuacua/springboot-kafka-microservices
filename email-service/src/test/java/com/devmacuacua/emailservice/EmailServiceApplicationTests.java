8a8bd0dd7441ad3e1536a2b04247ff05f3396d2e
Francisco Macuacua <macuacuafrancisco@gmail.com>
2025-03-30 09:37:48 +0200

    first commit

---
+++ email-service/src/test/java/com/devmacuacua/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmacuacua.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
